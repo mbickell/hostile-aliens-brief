c585632a804f1dc5b74b8aaa0779e451c9c552cf
Matthew Bickell <matthew.bickell@Nology.local>
2019-10-17 13:58:22 +0100

    Tweaking

--- main.js
+++ main.js
@@ -4,25 +4,18 @@ import {
   damageShipAndUpdateDisplay,
 } 
 from "./code.js"
-
-
 const shipData = [
   ["mothership", 100, 9, true, 1],
   ["defence", 80, 10, false, 5],
   ["attack", 45, 12, false, 8]
 ]
 let ships = generateShipData(shipData)
-
 insertShips(ships)
-
 let fireButton = document.getElementById("fire");
-
 fireButton.addEventListener("click", () => {
   damageShipAndUpdateDisplay(ships)
 })
-
 let resetButton = document.getElementById("reset");
-
 resetButton.addEventListener("click", () => {
   ships = generateShipData(shipData)
   insertShips(ships)
