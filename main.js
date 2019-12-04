9495654ea63a431f65fbc75253bce40996ed13a3
Matthew Bickell <matthew.bickell@Nology.local>
2019-12-04 14:11:48 +0000

    Fixed render issue

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 import {
   generateShipData,
-  insertShips, 
-  damageShipAndUpdateDisplay,
-} 
-from "./code.js"
+  insertShips,
+  damageShipAndUpdateDisplay
+} from "./code.js";
+
 const shipData = [
   ["mothership", 100, 9, true, 1],
   ["defence", 80, 10, false, 5],
   ["attack", 45, 12, false, 8]
-]
-let ships = generateShipData(shipData)
-insertShips(ships)
+];
+
+let ships = generateShipData(shipData);
 let fireButton = document.getElementById("fire");
-fireButton.addEventListener("click", () => {
-  damageShipAndUpdateDisplay(ships)
-})
 let resetButton = document.getElementById("reset");
+
+insertShips(ships);
+fireButton.addEventListener("click", () => {
+  damageShipAndUpdateDisplay(ships);
+});
+
 resetButton.addEventListener("click", () => {
-  ships = generateShipData(shipData)
-  insertShips(ships)
-})
+  ships = generateShipData(shipData);
+  insertShips(ships);
+});
